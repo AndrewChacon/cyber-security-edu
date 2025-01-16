2c87c65decd18b51d75f5b2333ae120fec7bba00
AndrewChacon <andrew.b.chacon@gmail.com>
2025-01-16 04:11:52 -0500

    using conditionals to allow us to select which tools we want to run on the scan

--- bug-bounty-bootcamp/building-your-own-recon-scripts/recon.sh
+++ bug-bounty-bootcamp/building-your-own-recon-scripts/recon.sh
@@ -3,10 +3,22 @@
 DOMAIN=$1
 DIRECTORY=${DOMAIN}_recon
 TODAY=$(date)
+echo "This scan was created on $TODAY."
 
 echo "Creating directory $DIRECTORY."
 mkdir $DIRECTORY
-nmap $DOMAIN | tee $DIRECTORY/nmap.txt
-echo "The results of nmap scan are stored in $DIRECTORY/nmap.txt"
-python3 dirsearch.py $DOMAIN | tee $DIRECTORY/dirsearch.txt
-echo "the results of dirsearch are stored in $DIRECTORY/dirsearch.txt"
+
+if [ $2 == 'nmap-only' ]
+then
+  nmap $DOMAIN | tee $DIRECTORY/nmap.txt
+  echo "The results of nmap scan are stored in $DIRECTORY/nmap.txt"
+elif [ $2 == 'dirsearch-only' ]
+then
+  python3 dirsearch.py $DOMAIN | tee $DIRECTORY/dirsearch.txt
+  echo "the results of dirsearch are stored in $DIRECTORY/dirsearch.txt"
+else
+  nmap $DOMAIN | tee $DIRECTORY/nmap.txt
+  echo "The results of nmap scan are stored in $DIRECTORY/nmap.txt"
+  python3 dirsearch.py $DOMAIN | tee $DIRECTORY/dirsearch.txt
+  echo "the results of dirsearch are stored in $DIRECTORY/dirsearch.txt"
+fi
