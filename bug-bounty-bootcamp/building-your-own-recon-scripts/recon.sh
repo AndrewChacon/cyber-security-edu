2a51d1f29c6b2bb602603dc67831ca16b6bea810
AndrewChacon <andrew.b.chacon@gmail.com>
2025-01-17 23:40:26 -0500

    fixing error with recon script and python script

--- bug-bounty-bootcamp/building-your-own-recon-scripts/recon.sh
+++ bug-bounty-bootcamp/building-your-own-recon-scripts/recon.sh
@@ -3,22 +3,44 @@
 DOMAIN=$1
 DIRECTORY=${DOMAIN}_recon
 TODAY=$(date)
+
+nmap_scan()
+{
+  nmap $DOMAIN | tee $DIRECTORY/nmap.txt
+  echo "The results of nmap scan are stored in $DIRECTORY/nmap.txt"
+
+}
+
+dirsearch_scan()
+{
+  python3 dirsearch.py $DOMAIN | tee $DIRECTORY/dirsearch.txt
+  echo "the results of dirsearch are stored in $DIRECTORY/dirsearch.txt"
+}
+
+crt_scan()
+{
+  curl "https://crt.sh/?q=$DOMAIN&output=json" -o $DIRECTORY/crt.txt
+  echo "The results of cert parsing is stored in $DIRECTORY/crt.txt"
+}
+
 echo "This scan was created on $TODAY."
 
 echo "Creating directory $DIRECTORY."
 mkdir $DIRECTORY
 
-if [ $2 == 'nmap-only' ]
-then
-  nmap $DOMAIN | tee $DIRECTORY/nmap.txt
-  echo "The results of nmap scan are stored in $DIRECTORY/nmap.txt"
-elif [ $2 == 'dirsearch-only' ]
-then
-  python3 dirsearch.py $DOMAIN | tee $DIRECTORY/dirsearch.txt
-  echo "the results of dirsearch are stored in $DIRECTORY/dirsearch.txt"
-else
-  nmap $DOMAIN | tee $DIRECTORY/nmap.txt
-  echo "The results of nmap scan are stored in $DIRECTORY/nmap.txt"
-  python3 dirsearch.py $DOMAIN | tee $DIRECTORY/dirsearch.txt
-  echo "the results of dirsearch are stored in $DIRECTORY/dirsearch.txt"
-fi
+case $2 in
+  nmap-only)
+    nmap_scan
+    ;;
+  dirsearch-only)
+    dirsearch_scan
+    ;;
+  crt-only)
+    crt_scan
+    ;;
+  *)
+    nmap_scan
+    dirsearch_scan
+    crt_scan
+    ;;
+esac
